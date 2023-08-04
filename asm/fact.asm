f7b8b7400c5494ea561dbf47262c2cab6781a63a
Saleh <sabasipyh@gmail.com>
2023-08-04 16:48:32 +0330

    fixed update prediction

--- asm/fact.asm
+++
@@ -1,3 +0,0 @@
-Part of Assignment
-Store 60'th number in factorial series in reg rx07
-
--- asm/fibo_bne.asm
+++ asm/fibo_bne.asm
@@ -1,9 +1,11 @@
-lui rx6, 1
-addi rx2, rx0, 20
-addi rx3, rx0, 2
-loop: add rx4, rx6, rx1
-addi rx6, rx1, 0
-addi rx1, rx4, 0
-beq rx3, rx2, loop
-addi rx3, rx3, 1
-end: add rx4, rx0, rx1
+li rx6, 1
+  li rx1, 1
+  lui rx0, 0
+  addi rx2, rx0, 30
+  addi rx3, rx0, 1
+loop:  add rx4, rx6, rx1
+  addi rx6, rx1, 0
+  addi rx1, rx4, 0
+  addi rx3, rx3, 1
+  bne rx3, rx2, loop
+  add rx4, rx0, rx1
